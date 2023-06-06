f8a13c3abe9250f63855f6e91233262e4183ceb1
David Castaneda <davidehcastaneda@gmail.com>
2023-06-06 11:21:27 -0400

    Initial Commit

---
+++ Homework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by David Castaneda on 6/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
